86426fa943d81438a164ce0d2bf50c054099f19d
akd13 <akankshitadash@yahoo.com>
2018-03-12 01:52:53 +0800

    Refactoring

--- pysat/SATSolver.py
+++ pysat/SATSolver.py
@@ -3,9 +3,6 @@ import re
 import copy
 from collections import OrderedDict
 
-clauses = []
-solved = {}
-
 def parse_input(): #open input file from command line
     parser = argparse.ArgumentParser()
     parser.add_argument('file')
@@ -14,118 +11,127 @@ def parse_input(): #open input file from command line
 def add_clauses(filename):
     file = open(filename, "r")
     for line in file:
-        m = re.search('^\s*((-)*\d+\s*)*(0)(\\n)$', line) #detect clause
-        if m!=None:
+        p = re.search('^\s*(p|c).*(\n)*$',line)
+        m = re.search('^\s*((-)*\d+\s*)*(0)(\\n)*$', line) #detect clause
+        if p == None and m == None:
+            print(line," is invalid!")
+            break;
+        elif m != None:
             raw_clause = m.group(0).split()
             size =  len(raw_clause)
-            if (raw_clause[size-1]==0):
-                raw_clause.remove(size-1)
+            if (raw_clause[size-1]=='0'):
+                raw_clause.pop()
 
             clause = [int(numeric_string) for numeric_string in raw_clause]
             clauses.append(clause)
     return clauses
 
-class Clause(object):
-
-    def __init__(self):
-        self.id = self.make_id()
-
-    @classmethod
-    def make_id(cls):
-        cls._num += 1
-        return cls._num
+def unit_propagation(clause, clause_set, literal_values, index_clause):
 
 
-def DPLL(clauseSet):
-    if (len(clauseSet) == 0):  # clause set empty
-        return True
-    for clause in clauseSet:  # contains an empty clause
-        if (len(clause) == 1):
-            return False
-    global solved
-    for clause in clauseSet:  # unit prop on unit clause
-        index = 0
-        if (len(clause) == 2):
-            item = clause[0]
-            if item < 0:
-                solved[-1 * item] = 0
-            else:
-                solved[item] = 1
-            clauseSet.pop(index)
-            index = 0
-            while (index != len(clauseSet)):
-                marked = False
-                for lit in clauseSet[index]:
-                    if lit == item:
-                        marked = True
-                        break
-                    elif (lit == (-1 * item)):
-                        clauseSet[index].remove(lit)
-                if marked:
-                    clauseSet.pop(index)
-                    index = index - 1
-                index = index + 1
-            return DPLL(copy.deepcopy(clauseSet))
-
-    shortClause = clauseSet[0]
-    for clause in clauseSet:  # choose a variable by shortest remaining
-        if len(shortClause) > len(clause):
-            shortClause = clause
-    item = shortClause[0]
-    tempSet = copy.deepcopy(clauseSet)
-    tempSet.remove(shortClause)
+    pure_literal = clause[0]
+    if (pure_literal < 0): #if negative
+        literal_values[-1 * pure_literal] = 0
+    else:
+        literal_values[pure_literal] = 1 #if positive
+    clause_set.pop(index_clause) #remove clause containing the pure literal
 
     index = 0
-    while (index != len(tempSet)):
-        marked = False
-        for lit in tempSet[index]:
-            if lit == item:
-                marked = True
+    while (index != len(clause_set)):
+        is_solved = False
+
+        for lit in clause_set[index]:
+            if (lit == pure_literal):
+                is_solved = True
                 break
-            elif (lit == (-1 * item)):
-                tempSet[index].remove(lit)
-        if marked:
-            tempSet.pop(index)
+            elif (lit == (-1 * pure_literal)):
+                clause_set[index].remove(lit) #remove the pure-literal's negation from a clause
+
+        if (is_solved): #remove the clause (which is now true by default due to pure-literal) from F
+            clause_set.pop(index)
+            index-=1
+        index+=1
+
+
+def DPLL(clause_set, literal_values):
+
+    if (not(clause_set)): # set of clauses are empty
+        return True
+
+    for clause in clause_set:
+        if (not(clause)):  # if there is an empty clause
+            return False
+
+    for clause in clause_set:  # unit prop on unit clause
+        index = 0
+        if (len(clause) == 1):
+            unit_propagation(clause, clause_set, literal_values, index)
+            return DPLL(clause_set, literal_values)
+
+    shortest_clause = clause_set[0]
+    for clause in clause_set:  # choose a variable by shortest remaining
+        if len(shortest_clause) > len(clause):
+            shortest_clause = clause
+    first_element = shortest_clause[0]
+    temp_clauseset = clause_set
+    temp_clauseset.remove(shortest_clause)
+
+    index = 0
+    while (index != len(temp_clauseset)):
+        is_solved = False
+        for lit in temp_clauseset[index]:
+            if lit == first_element:
+                is_solved = True
+                break
+            elif (lit == (-1 * first_element)):
+                temp_clauseset[index].remove(lit)
+        if is_solved:
+            temp_clauseset.pop(index)
             index = index - 1
         index = index + 1
-    tempSolve = copy.deepcopy(solved)
-    done = DPLL(tempSet)
+    temp_literal_values = literal_values
+    done = DPLL(temp_clauseset, literal_values)
+
     if done:
-        if item < 0:
-            solved[-1 * item] = 0
+        if first_element < 0:
+            literal_values[-1 * first_element] = 0
         else:
-            solved[item] = 1
+            literal_values[first_element] = 1
         return done
     else:
-        solved = copy.deepcopy(tempSolve)
-        item = (-1 * item)
-        tempSet = copy.deepcopy(clauseSet)
+        literal_values = temp_literal_values
+        first_element = (-1 * first_element)
+        temp_clauseset = copy.deepcopy(clause_set)
         index = 0
-        while (index != len(tempSet)):
-            marked = False
-            for lit in tempSet[index]:
-                if lit == item:
-                    marked = True
+        while (index != len(temp_clauseset)):
+            is_solved = False
+            for lit in temp_clauseset[index]:
+                if lit == first_element:
+                    is_solved = True
                     break
-                elif (lit == (-1 * item)):
-                    tempSet[index].remove(lit)
-            if marked:
-                tempSet.pop(index)
+                elif (lit == (-1 * first_element)):
+                    temp_clauseset[index].remove(lit)
+            if is_solved:
+                temp_clauseset.pop(index)
                 index = index - 1
             index = index + 1
-        if item < 0:
-            solved[-1 * item] = 0
+        if first_element < 0:
+            literal_values[-1 * first_element] = 0
         else:
-            solved[item] = 1
-    return DPLL(tempSet)
+            literal_values[first_element] = 1
+    return DPLL(temp_clauseset)
 
+clauses = []
+literal_values = {}
 clauses = add_clauses(parse_input())
-solver = DPLL(copy.deepcopy(clauses))
-solved = OrderedDict(sorted(solved.items()))
+can_solve = DPLL(clauses, literal_values)
+literal_values = OrderedDict(sorted(literal_values.items()))
 
-if solver:
-    for s in solved:
-        print(s, solved[s])
+if can_solve:
+    for s in literal_values:
+        print(s, literal_values[s])
 else:
+    for s in literal_values:
+        print(s, literal_values[s])
     print("UNSATISFIABLE")
 
