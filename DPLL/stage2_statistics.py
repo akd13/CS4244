7200b443a2c8323c77f85bad5ca858cc8247ece7
ShumShum93 <a0121823@u.nus.edu>
2018-05-11 16:59:28 +0800

    Stage 2

---
+++ DPLL/stage2_statistics.py
@@ -0,0 +1,25 @@
+from DPLL.cdcl import SATSolverCDCL
+from DPLL.stage2_generator import gen_cnf
+import numpy as np
+
+N = 150
+R = np.arange(0, 10.2, 0.2)
+K = [3, 4, 5]
+
+for k in K:
+	for r in R:
+		print("K:{0}     R:{1}".format(k, r))
+		num_total = 0
+		num_sat = 0
+
+		# Generate 50 formulas
+		for i in range(50):
+			cnf = gen_cnf(N, k, r)
+			solver = SATSolverCDCL(cnf, N, 'VSIDS')
+			result = solver.solve_test()
+			num_total += 1
+			if result == 'satisfied':
+				num_sat += 1
+		print("Stats: {0}".format(num_sat/num_total))
+
+print(gen_cnf(N, 0.2, 3))
