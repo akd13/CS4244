bdf82067292f25d30f875483522df598cce28248
ShumShum93 <a0121823@u.nus.edu>
2018-05-11 18:23:50 +0800

    Stage2

--- DPLL/stage2_statistics.py
+++ DPLL/stage2_statistics.py
@@ -3,24 +3,29 @@ from DPLL.stage2_generator import gen_cnf
 import numpy as np
 import gc
 import pycosat
+import pickle
 
 N = 150
 R = np.arange(0.2, 10.2, 0.2)
 K = [3, 4, 5]
 
-for k in K:
-	for r in R:
-		print("K:{0}     R:{1}".format(k, r))
-		num_total = 0
-		num_sat = 0
+file = "stats.csv"
 
-		# Generate 50 formulas
-		for i in range(50):
-			cnf = gen_cnf(N, k, r)
-			result = pycosat.solve(cnf)
-			num_total += 1
-			if result != 'UNSAT':
-				num_sat += 1
-		print("Stats: {0}".format(num_sat/num_total))
+with open(file, 'w') as f:
+	f.write("K,R,Prob of Sat")
+	for k in K:
+		for r in R:
+			num_total = 0
+			num_sat = 0
 
-print(gen_cnf(N, 0.2, 3))
+			# Generate 50 formulas
+			for i in range(50):
+				cnf = gen_cnf(N, k, r)
+				result = pycosat.solve(cnf)
+				num_total += 1
+				if result != 'UNSAT':
+					num_sat += 1
+			probability = num_sat/num_total
+			print("K:{0}     R:{1}".format(k, r), "  Stats: {0}".format(probability))
+			f.write('{0},{1},{2}'.format(k, r, probability))
+print("Done")
